b87076ce366c508044915f8ce6e6beb8120d6442
Vasilii Dubik <vdubik@azds.com>
2024-11-21 23:10:27 +0300

    Move reused logic to separate method

--- components/LanguageToggle.js
+++ components/LanguageToggle.js
@@ -135,14 +135,18 @@ class LanguageToggle {
         .replace(".", "° ")}`;
     }
 
-    const currentDayIndex = new Date().getDay();
-    const firstDayIndex = (currentDayIndex + 1) % 7;
-    const secondDayIndex = (currentDayIndex + 2) % 7;
-    const lastDayIndex = (currentDayIndex + 3) % 7;
+    const firstDayIndex = this.getDayOfWeekIndex(1);
+    const secondDayIndex = this.getDayOfWeekIndex(2);
+    const lastDayIndex = this.getDayOfWeekIndex(3);
 
     this.firstDaylang.textContent = translations.days[firstDayIndex];
     this.secondDaylang.textContent = translations.days[secondDayIndex];
     this.lastDaylang.textContent = translations.days[lastDayIndex];
+  }
+  getDayOfWeekIndex(value) {
+    const currentDayIndex = new Date().getDay();
+
+    return (currentDayIndex + value) % 7;
   }
 }
 
